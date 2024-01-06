d6cb269639c32aae1b4e5b2ae27d79ac9bbf9c2b
gerasimovtltsu <theavg@yandex.ru>
2024-01-06 14:28:52 +0400

    feat: add getting config from file

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+
+from config import DevelopmentConfig, ProductionConfig, get_config
+
 from routers.main_router import main_router
 
 app = Flask(__name__)
+app.config.from_object(get_config('development'))
+secret_key = app.config['SECRET_KEY']
 
 app.register_blueprint(main_router)
 
